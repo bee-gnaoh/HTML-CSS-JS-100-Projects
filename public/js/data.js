c2a6df10057053dc2491d3bc6b6575df1cc75731
hoangtv <hoangtv@dyno>
2024-06-09 21:49:32 +0700

    feat(p-9): 4 card feature

--- public/js/data.js
+++ public/js/data.js
@@ -1,43 +1,48 @@
 const data = [
     {
-        path: './01_Loading_Animation',
+        path: '/01_Loading_Animation',
         title: 'Loading Animation',
         preview: 'preview.jpeg'
     },
     {
-        path: './02_Custom_Scrollbar',
+        path: '/02_Custom_Scrollbar',
         title: 'Custom Scrollbar',
         preview: 'preview.jpeg'
 
     },
     {
-        path: './03_Image_Hover_Effects',
+        path: '/03_Image_Hover_Effects',
         title: 'Image Hover Effects',
         preview: 'preview.jpeg'
     },
     {
-        path: './04_Tooltip',
+        path: '/04_Tooltip',
         title: 'Tooltip',
         preview: 'preview.jpeg'
     },
     {
-        path: './05_Video_Trailer_Popup',
+        path: '/05_Video_Trailer_Popup',
         title: 'Video Trailer Popup',
         preview: 'preview.jpeg'
     },
     {
-        path: './06_Blog_Preview_Card',
+        path: '/06_Blog_Preview_Card',
         title: 'Blog Preview Card',
         preview: 'preview.jpeg'
     },
     {
-        path: './07_Results_Summary_Component',
+        path: '/07_Results_Summary_Component',
         title: 'Results Summary Component',
         preview: 'preview.jpg'
     },
     {
-        path: './08_QR_Code_Component',
+        path: '/08_QR_Code_Component',
         title: 'QR Code Component',
+        preview: 'preview.jpg'
+    },
+    {
+        path: '/09_Four_Card_Feature_Section_Master',
+        title: 'Four Card Feature Section Master',
         preview: 'preview.jpg'
     },
 ]
--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,16 @@
 const projects = document.querySelector('.projects')
 
 window.onload = async () => {
-    console.log(data);
     data.forEach(item => {
         projects.innerHTML += `
             <div>
-                <img src="${item.path}/${item.preview}">
+                <img src="./projects${item.path}/${item.preview}">
                 <div>
                     <p>${item.title}</p>
-                    <a href="${item.path}/index.html">Preview</a>
+                    <a href="./projects${item.path}/index.html">Preview</a>
                 </div>
             </div>
         `
 
     })
 }
-
-// async function init() {
-//     const directoryHandle = await window.showDirectoryPicker()
-//     console.log(directoryHandle);
-// }
